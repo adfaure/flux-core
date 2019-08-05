86a82a20f65667861f228d7bb0a60f8df543fdaf
Stephen Herbein <stephen272@gmail.com>
2019-08-05 09:28:04 -0700

    bootstrap: (INPROGRESS) refactor

--- src/bindings/python/flux/job.py
+++ src/bindings/python/flux/job.py
@@ -8,9 +8,20 @@
 # SPDX-License-Identifier: LGPL-3.0
 ###############################################################
 
+import re
+import math
+import json
 import errno
+from datetime import timedelta
+import collections
+
+try:
+    collectionsAbc = collections.abc
+except AttributeError:
+    collectionsAbc = collections
 
 import six
+import yaml
 
 from flux.wrapper import Wrapper
 from flux.util import check_future_error
@@ -52,3 +63,200 @@ def submit_get_id(future):
 def submit(flux_handle, jobspec, priority=lib.FLUX_JOB_PRIORITY_DEFAULT, flags=0):
     future = submit_async(flux_handle, jobspec, priority, flags)
     return submit_get_id(future)
+
+
+class JobSpec:
+    def __init__(self, yaml_stream):
+        self.jobspec = yaml.safe_load(yaml_stream)
+
+    def _create_resource(self, res_type, count, with_child=[]):
+        assert isinstance(
+            with_child, collectionsAbc.Sequence
+        ), "child resource must be a sequence"
+        assert not isinstance(
+            with_child, six.string_types
+        ), "child resource must not be a string"
+        assert count > 0, "resource count must be > 0"
+
+        res = {"type": res_type, "count": count}
+
+        if len(with_child) > 0:
+            res["with"] = with_child
+        return res
+
+    def _create_slot(self, label, count, with_child):
+        slot = self._create_resource("slot", count, with_child)
+        slot["label"] = label
+        return slot
+
+    def _parse_fsd(self, s):
+        m = re.match(r".*([smhd])$", s)
+        try:
+            n = float(s[:-1] if m else s)
+        except:
+            raise ValueError("invalid Flux standard duration")
+        unit = m.group(1) if m else "s"
+
+        if unit == "m":
+            seconds = timedelta(minutes=n).total_seconds()
+        elif unit == "h":
+            seconds = timedelta(hours=n).total_seconds()
+        elif unit == "d":
+            seconds = timedelta(days=n).total_seconds()
+        else:
+            seconds = n
+        if seconds < 0 or math.isnan(seconds) or math.isinf(seconds):
+            raise ValueError("invalid Flux standard duration")
+        return seconds
+
+    @property
+    def duration(self):
+        try:
+            return self.jobspec["attributes"]["system"]["duration"]
+        except KeyError:
+            return None
+
+    @duration.setter
+    def set_duration(self, duration):
+        """
+        Assign a time limit to the job.  The duration may be:
+        - a float in seconds
+        - a string in Flux Standard Duration
+        A duration of zero is interpreted as "not set".
+        """
+        if isinstance(duration, six.string_types):
+            t = self._parse_fsd(duration)
+        elif isinstance(duration, float):
+            t = duration
+        else:
+            raise ValueError("duration must be a float or string")
+        if t < 0:
+            raise ValueError("duration must not be negative")
+        if math.isnan(t) or math.isinf(t):
+            raise ValueError("duration must be a normal, finite value")
+        self.jobspec["attributes"]["system"]["duration"] = t
+
+    def set_cwd(self, cwd):
+        """
+        Set working directory of job.
+        """
+        if not isinstance(cwd, six.string_types):
+            raise ValueError("cwd must be a string")
+        self.jobspec["attributes"]["system"]["cwd"] = cwd
+
+    def set_environment(self, environ):
+        """
+        Set (entire) environment of job.
+        """
+        if not isinstance(environ, collectionsAbc.Mapping):
+            raise ValueError("environment must be a mapping")
+        self.jobspec["attributes"]["system"]["environment"] = environ
+
+    def _set_treedict(self, d, key, val):
+        """
+        _set_treedict(d, "a.b.c", 42) is like d[a][b][c] = 42
+        but levels are created on demand.
+        """
+        path = key.split(".", 1)
+        if len(path) == 2:
+            self._set_treedict(d.setdefault(path[0], {}), path[1], val)
+        else:
+            d[key] = val
+
+    def setattr(self, key, val):
+        """
+        set job attribute
+        """
+        self._set_treedict(self.jobspec, "attributes." + key, val)
+
+    def setattr_shopt(self, key, val):
+        """
+        set job attribute: shell option
+        """
+        self.setattr("system.shell.options." + key, val)
+
+    def dumps(self):
+        return json.dumps(self.jobspec)
+
+    @property
+    def resources(self):
+        return self.jobspec.get("resources", None)
+
+    @property
+    def tasks(self):
+        return self.jobspec.get("tasks", None)
+
+    @property
+    def attributes(self):
+        return self.jobspec.get("attributes", None)
+
+    @property
+    def version(self):
+        return self.jobspec.get("version", None)
+
+    def count_num_type(self, res_type):
+        def _count_helper(curr_res, res_type):
+            resource_count = curr_res["count"]
+            if curr_res["type"] == res_type:
+                return resource_count
+            num_in_children = sum(
+                _count_helper(r, res_type) for r in curr_res.get("with", [])
+            )
+            return resource_count * num_in_children
+
+        return sum(_count_helper(r, res_type) for r in self.jobspec["resources"])
+
+    @property
+    def total_num_nodes(self):
+        return self.count_num_type("node")
+
+    @property
+    def total_num_cores(self):
+        return self.count_num_type("core")
+
+
+class JobSpecV1(JobSpec):
+    def __init__(
+        self, command, num_tasks=1, cores_per_task=1, gpus_per_task=None, num_nodes=None
+    ):
+        """
+        Constructor builds the minimum legal v1 jobspec.
+        Use setters to assign additional properties.
+        """
+        if not isinstance(command, (list, tuple)) or not command:
+            raise ValueError("command must be a non-empty list or tuple")
+        if not isinstance(num_tasks, int) or num_tasks < 1:
+            raise ValueError("task count must be a integer >= 1")
+        if not isinstance(cores_per_task, int) or cores_per_task < 1:
+            raise ValueError("cores per task must be an integer >= 1")
+        if gpus_per_task is not None:
+            if not isinstance(gpus_per_task, int) or gpus_per_task < 1:
+                raise ValueError("gpus per task must be an integer >= 1")
+        if num_nodes is not None:
+            if not isinstance(num_nodes, int) or num_nodes < 1:
+                raise ValueError("node count must be an integer >= 1 (if set)")
+            if num_nodes > num_tasks:
+                raise ValueError("node count must not be greater than task count")
+        children = [self._create_resource("core", cores_per_task)]
+        if gpus_per_task is not None:
+            children.append(self._create_resource("gpu", gpus_per_task))
+        if num_nodes is not None:
+            num_slots = int(math.ceil(num_tasks / float(num_nodes)))
+            if num_tasks % num_nodes != 0:
+                # N.B. uneven distribution results in wasted task slots
+                task_count_dict = {"total": num_tasks}
+            else:
+                task_count_dict = {"per_slot": 1}
+            slot = self._create_slot("task", num_slots, children)
+            resource_section = self._create_resource("node", num_nodes, [slot])
+        else:
+            task_count_dict = {"per_slot": 1}
+            slot = self._create_slot("task", num_tasks, children)
+            resource_section = slot
+
+        self.jobspec = {
+            "version": 1,
+            "resources": [resource_section],
+            "tasks": [{"command": command, "slot": "task", "count": task_count_dict}],
+            "attributes": {"system": {"duration": 0}},
+        }
--- src/cmd/flux-bootstrap.py
+++ src/cmd/flux-bootstrap.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python
-
 from __future__ import print_function
 
 import os
 import sys
-import yaml
+import shutil
 import inspect
+import logging
 import argparse
-from datetime import datetime
+import tempfile
+import itertools
 from enum import Enum
 from subprocess import Popen, PIPE
+
+from flux.job import JobSpec
+from flux import util
 
 
 class ResourceManager(object):
@@ -17,10 +20,8 @@ class ResourceManager(object):
 
     def __init__(self, jobspec):
         """Initializes with a flux jobspec
-
         """
-
-        self.jobspec = yaml.safe_load(jobspec)
+        self.jobspec = JobSpec(jobspec)
 
     def args(self):
         """Return the set of args to use, translating if available
@@ -31,77 +32,116 @@ class ResourceManager(object):
         Calls every defined map method with the loaded jobspec
         """
 
-        args = (m() for name, m
-                in inspect.getmembers(self, inspect.ismethod)
-                if name.startswith("map_"))
+        args = (
+            m()
+            for name, m in inspect.getmembers(self, inspect.ismethod)
+            if name.startswith("map_")
+        )
         return [arg for arg in args if arg is not None]
 
     def submit(self, dry_run=False):
         raise NotImplementedError("must implement submit")
 
 
+def seconds_to_walltime(seconds):
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return "{:02d}:{:02d}:{:02d}".format(int(hours), int(minutes), int(seconds))
+
+
 class Slurm(ResourceManager):
 
     default_shell = "/bin/sh"
 
-    def job_script(self, args):
+    def job_script(self, args, jobspec_str):
         """Takes a set of slurm args and outputs a jobscript
         """
-
-        attributes = self.jobspec["attributes"]
-        shell = self.default_shell
-
-        if "user" in attributes:
-            shell = attributes["user"].get("shell", shell)
+        try:
+            shell = self.jobspec.attributes["user"]["shell"]
+        except KeyError:
+            shell = self.default_shell
 
         script = "#!{shell}\n".format(shell=shell)
-        script += '\n'.join("#SBATCH " + arg for arg in args) + '\n'
-        script += "srun flux start\n"
+        script += (
+            "\n".join("#SBATCH " + " ".join(arg_tuple) for arg_tuple in args) + "\n"
+        )
+        script += "echo '{}' > /tmp/test.yaml\n".format(jobspec_str)
+        script += "srun -N ${SLURM_JOB_NUM_NODES} -n ${SLURM_NTASKS} --mpibind=off --cpu-bind=none flux start bash -c 'flux job attach $(flux job submit /tmp/test.yaml)'\n"
 
         return script
 
-    def map_resource_cores(self):
-        # TODO: right way to calculate this..
-        # return "-c {:d}".format(num_cores)
-        return
+    def interactive_command(self, args, jobspec_str):
+        command = ["srun", "--mpibind=off", "--cpu-bind=none"]
+        command.extend(list(itertools.chain.from_iterable(args)))
+        command.extend(
+            [
+                "flux",
+                "start",
+                "bash",
+                "-c",
+                "flux job attach $(flux job submit <(echo '{}')".format(jobspec_str),
+            ]
+        )
+        return command
 
-    def map_task_tasks(self):
-        pass
+    def map_mincpus_per_node(self):
+        return [
+            "--mincpus={}".format(
+                self.jobspec.total_num_cores / self.jobspec.total_num_nodes
+            )
+        ]
+
+    # def map_resource_cores(self):
+    #     '''This does not request the exact number of cores per task that the user job
+    #     requires. This argument, in concert with `-c` ensures that we get enough
+    #     total cores for the user job.
+    #     '''
+    #     return "-c {:d}".format(
+
+    # def map_task_tasks(self):
+    #     '''This does not request the actual number of tasks that the user job
+    #     requires. This argument, in concert with `-c` ensures that we get enough
+    #     total cores for the user job.
+    #     '''
+    #     return "-n {:d}".format(self.jobspec.total_num_nodes)
 
     def map_resource_nodes(self):
-        # Assuming (for now) [slots] -> nodes -> cores
-        count = 0
-        for resource in self.jobspec["resources"]:
-            resource_type = resource["type"]
-            if resource_type == "slot" or resource_type == "node":
-                count += resource["count"]
-
-            # check to see if any nodes are defined in "with"
-            node_count = sum(r.get("count", 0)
-                             for r in resource.get("with", [])
-                             if r["type"] == "node")
-
-            node_count = 1 if node_count == 0 else node_count
-            count *= node_count
-
-        return "-N {:d}".format(count)
+        nnodes = self.jobspec.total_num_nodes
+        if nnodes > 0:
+            return ["-N", "{:d}".format(nnodes)]
+        return None
 
     def map_attribute_system(self):
-        args = []
-        system = self.jobspec["attributes"].get("system", {})
-        return "-t {}".format(system["duration"])
+        duration = self.jobspec.duration
+        if duration is None:
+            return None
+        else:
+            # the duration in JobSpec is stored as seconds,
+            # convert back to Slurm-style D-HH:MM:SS
+            walltime = seconds_to_walltime(duration)
+            return ["-t", "{}".format(walltime)]
 
     def submit(self, dry_run=False):
-        script = self.job_script(self.args())
+        # command = self.interactive_command(self.args(), self.jobspec.dumps())
+        # if dry_run:
+        #    print(command)
+        #    return
+        #
+        # self.proc = Popen(command, stdin=None, stdout=PIPE, stderr=PIPE)
+        # out, err = self.proc.communicate()
+        # print(err, file=sys.stderr)
+        # print(out)
+
+        script = self.job_script(self.args(), self.jobspec.dumps())
         if dry_run:
             print(script)
             return
-        cmd = ["sbatch"]
-
-        # TODO: is subprocess the right way to go here? pexpect would be
-        # easier to use, but add another dependency...
-        # self.proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        # out, err = self.proc.communicate(input=script)
+        self.proc = Popen(["sbatch"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        out, err = self.proc.communicate(input=script)
+        if len(err) > 0:
+            print(err, file=sys.stderr)
+        if len(out) > 0:
+            print(out)
 
 
 class LSF(ResourceManager):
@@ -112,9 +152,14 @@ class Cobalt(ResourceManager):
     pass
 
 
-ResourceManagers = Enum("ResourceManagers",
-                        {rm.__name__: rm for rm in
-                         ResourceManager.__subclasses__()})
+class Flux(ResourceManager):
+    pass
+
+
+ResourceManagers = Enum(
+    "ResourceManagers", {rm.__name__: rm for rm in ResourceManager.__subclasses__()}
+)
+
 
 def create_manager(manager_type, jobspec):
     """Create a new resource manager given a manager_type"""
@@ -129,14 +174,16 @@ def detect_manager(types):
     """Detect the resource manager on this host"""
 
     def which(cmd):
-        all_paths = (os.path.join(path, cmd) for path in
-                     os.environ["PATH"].split(os.pathsep))
-
-        return any(
-            os.access(path, os.X_OK) and os.path.isfile(path)
-            for path in all_paths
+        all_paths = (
+            os.path.join(path, cmd) for path in os.environ["PATH"].split(os.pathsep)
         )
 
+        return any(
+            os.access(path, os.X_OK) and os.path.isfile(path) for path in all_paths
+        )
+
+    if os.getenv("FLUX_URI") is not None:
+        return types.Flux
     if which("bsub"):
         return types.LSF
     elif which("salloc"):
@@ -145,6 +192,7 @@ def detect_manager(types):
         return types.Cobalt
     else:
         raise OSError("unable to find a resource manager on this system")
+
 
 def bootstrap(jobspec, dry_run=False):
     """Detect a resource manager and start flux with a given jobspec"""
@@ -158,14 +206,28 @@ def bootstrap(jobspec, dry_run=False):
     sys.exit(0)
 
 
-if __name__ == "__main__":
+logger = logging.getLogger("flux-bootstrap")
+
+
+@util.CLIMain(logger)
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-d",
+        "-n",
         "--dry-run",
         action="store_true",
-        help="""Print the submission command or script"""
+        help="""Print the submission command or script""",
     )
     parser.add_argument("jobspec")
-    args = parser.parse_args()
-    bootstrap(args.jobspec, dry_run=args.dry_run)
+    cli_args = parser.parse_args()
+    if cli_args.jobspec == "-":
+        logger.warn("Reading jobspec from stdin")
+        jobspec = sys.stdin.readline()
+    else:
+        with open(cli_args.jobspec, "r") as infile:
+            jobspec = infile.readline()
+    bootstrap(jobspec, dry_run=cli_args.dry_run)
+
+
+if __name__ == "__main__":
+    main()
